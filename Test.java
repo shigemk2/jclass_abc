e1b7393368d617af33b806ff6ef0aecbdaf4d12b
shigemk2 <i.am.shige@gmail.com>
2014-12-20 17:54:05 +0900

    逆コンパイル

--- Test.java
+++ Test.java
@@ -1,14 +1,13 @@
 class Test {
     public static void main(String[] argv) {
-        System.out.println(fib(10));
+        System.out.println("" + fib(10));
     }
 
     private static int fib(int x) {
-        if (x == 0) {
-            return 0;
-        } else if (x == 1 || x == 2) {
-            return 1;
+        if (x <= 1) {
+            return x;
+        } else {
+            return fib(x - 2) + fib(x - 1);
         }
-        return fib(x - 2) + fib(x - 1);
     }
 }
